325f5a0941101c3561506050bb68b344f58105f3
Daniel Jansen <44923461+xoladone@users.noreply.github.com>
2019-09-05 12:41:11 +0200

    Update function.js
    added timeout, so looking for a server is limited

--- function.js
+++ function.js
@@ -6,6 +6,7 @@ $urlToPing = "https://example.com/aFile.xyz;
     let $xhr = new XMLHttpRequest();
     $xhr.open('GET', $urlToPing, true);
     $xhr.setRequestHeader('Cache-Control', 'no-cache');
+    $xhr.timeout = 3000; // Set timeout to 3 seconds (3000 milliseconds)
     $xhr.addEventListener('readystatechange', function() {
       if ($xhr.status === 200 && $xhr.readyState === 4) {
         $callback(true, $optional);
